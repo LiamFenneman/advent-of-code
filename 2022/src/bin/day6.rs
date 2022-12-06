ebc2c8a15d1cd282d4cf3415809646dc0b05a76d
LiamFenneman <liamfennemanbusiness@gmail.com>
2022-12-06 18:25:28 +1300

    Day 6 Part 1

---
+++ 2022/src/bin/day6.rs
@@ -0,0 +1,50 @@
+// Advent of Code 2022
+// Liam Fenneman
+//
+// Day 6
+//
+// https://adventofcode.com/2022/day/6
+#![allow(unused)]
+
+fn any_chars_equal(window: Vec<char>) -> bool {
+    for (ai, a) in window.iter().enumerate() {
+        for (bi, b) in window.iter().enumerate() {
+            // dont compare the same index of the window
+            if ai == bi { continue; }
+            if a == b { return true; }
+        }
+    }
+
+    false
+}
+
+fn part1(chars: Vec<char>) -> usize {
+    // to process we start at index 4 since this is the first time that
+    // we are able to have a marker (marker is 4 unqiue chars in a row)
+
+    for (i, &c) in chars.iter().enumerate().skip(3) {
+        let window = vec![chars[i-3], chars[i-2], chars[i-1], c];
+
+        // are all chars unique?
+        // no? continue looping
+        if any_chars_equal(window) {
+            continue;
+        }
+
+        // otherwise
+        return i+1;
+    }
+
+    usize::MAX
+}
+
+fn main() {
+    let chars = std::io::stdin()
+        .lines()
+        .filter_map(|l| l.ok())
+        .flat_map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let ret = part1(chars.clone());
+    println!("Part 1: {}", ret);
+}
